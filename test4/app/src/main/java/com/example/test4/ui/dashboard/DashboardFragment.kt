ea086fac146cdcad18be6c8fc413ddea2c1ad064
choseongje <hangoy0606@gmail.com>
2024-06-27 20:42:05 +0900

    이미지 파일 변경

--- test4/app/src/main/java/com/example/test4/ui/dashboard/DashboardFragment.kt
+++ test4/app/src/main/java/com/example/test4/ui/dashboard/DashboardFragment.kt
@@ -24,11 +24,9 @@ class DashboardFragment : Fragment() {
         R.drawable.image1, R.drawable.image2, R.drawable.image3,
         R.drawable.image4, R.drawable.image5, R.drawable.image6,
         R.drawable.image7, R.drawable.image8, R.drawable.image9,
-        R.drawable.image1, R.drawable.image1, R.drawable.image1,
-        R.drawable.image1, R.drawable.image1, R.drawable.image1,
-        R.drawable.image1, R.drawable.image1, R.drawable.image1,
-        R.drawable.image1, R.drawable.image1, R.drawable.image1,
-        R.drawable.image1, R.drawable.image1, R.drawable.image1
+        R.drawable.image1, R.drawable.image2, R.drawable.image3,
+        R.drawable.image4, R.drawable.image5, R.drawable.image6,
+        R.drawable.image7, R.drawable.image8, R.drawable.image9
     )
 
     override fun onCreateView(
