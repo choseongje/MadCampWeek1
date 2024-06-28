e83fa5c606c9303f95dfe405bf1e916135346673
GunminKim <guninkim12@korea.ac.kr>
2024-06-28 15:40:37 +0900

    real final

--- test4/app/src/main/java/com/example/test4/ui/notifications/NotificationsFragment.kt
+++ test4/app/src/main/java/com/example/test4/ui/notifications/NotificationsFragment.kt
@@ -1,6 +1,5 @@
 package com.example.test4.ui.notifications
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -15,11 +14,20 @@ import com.pierfrancescosoffritti.androidyoutubeplayer.core.player.PlayerConstan
 import androidx.fragment.app.Fragment
 
 import com.example.test4.databinding.FragmentNotificationsBinding
+import com.example.test4.R
+import android.widget.ImageView
+import android.widget.TextView
 
 class NotificationsFragment : Fragment() {
 
     private var _binding: FragmentNotificationsBinding? = null
     private val binding get() = _binding!!
+
+    private val images = arrayOf(
+        R.drawable.cat1,
+        R.drawable.cat2
+    )
+    private var currentIndex = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -35,7 +43,7 @@ class NotificationsFragment : Fragment() {
         youTubePlayerView.addYouTubePlayerListener(object : AbstractYouTubePlayerListener() {
             override fun onReady(youTubePlayer: YouTubePlayer) {
                 // 유효한 비디오 ID를 사용하여 비디오를 로드합니다.
-                val videoId = "hbcGx4MGUMg" // 여기에 실제 비디오 ID를 넣으세요
+                val videoId = "CJkrxF6DiJs" // 여기에 실제 비디오 ID를 넣으세요
                 youTubePlayer.loadVideo(videoId, 0f)
             }
             override fun onError(youTubePlayer: YouTubePlayer, error: PlayerError) {
@@ -43,6 +51,21 @@ class NotificationsFragment : Fragment() {
                 Log.e("YouTubePlayer", "Error occurred: $error")
             }
         })
+
+        // 이미지 뷰와 텍스트 뷰 설정
+        val imageView: ImageView = binding.imageView
+        val textView: TextView = binding.textView
+
+        imageView.setImageResource(images[currentIndex])
+        textView.text = "  $currentIndex" // 초기화 시 인덱스 설정
+        imageView.setOnClickListener {
+            // 다음 이미지로 전환
+            currentIndex = (currentIndex + 1)
+            imageView.setImageResource(images[currentIndex % images.size])
+            // 텍스트 뷰 업데이트
+            textView.text = "   $currentIndex"
+        }
+
         return root
     }
 
