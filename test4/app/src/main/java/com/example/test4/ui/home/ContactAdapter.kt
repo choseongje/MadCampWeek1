d9a6e8e5f1cebd7ad73b6eb7bcad7172872b71ec
choseongje <hangoy0606@gmail.com>
2024-07-02 16:35:34 +0900

    연락처 탭 UI 개선 및 통화 앱으로 연결

--- test4/app/src/main/java/com/example/test4/ui/home/ContactAdapter.kt
+++ test4/app/src/main/java/com/example/test4/ui/home/ContactAdapter.kt
@@ -2,7 +2,9 @@ package com.example.test4.ui.home
 
 import android.app.AlertDialog
 import android.content.Context
+import android.content.Intent
 import android.content.SharedPreferences
+import android.net.Uri
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -24,7 +26,7 @@ class ContactAdapter(private val contacts: MutableList<Contact>, private val sha
         holder.nameTextView.text = contact.name
         holder.phoneTextView.text = contact.phoneNumber
 
-        holder.deleteButton.setOnClickListener {
+        holder.itemView.setOnLongClickListener {
             AlertDialog.Builder(context)
                 .setTitle("연락처 삭제")
                 .setMessage("이 연락처를 삭제하시겠습니까?")
@@ -33,13 +35,19 @@ class ContactAdapter(private val contacts: MutableList<Contact>, private val sha
                 }
                 .setNegativeButton("아니오", null)
                 .show()
+            true
+        }
+
+        holder.callButton.setOnClickListener{
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:${contact.phoneNumber}"))
+            context.startActivity(intent)
         }
     }
 
     inner class ContactViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val nameTextView: TextView = itemView.findViewById(R.id.nameTextView)
         val phoneTextView: TextView = itemView.findViewById(R.id.phoneTextView)
-        val deleteButton: ImageButton = itemView.findViewById(R.id.deleteButton)
+        val callButton: ImageButton = itemView.findViewById(R.id.callButton)
     }
 
     interface OnContactDeletedListener{
